08790a71a5ba60ea00b5899c8e372a371f562681
kanxer <user.kanxer@gmail.com>
2024-06-25 14:57:09 +0530

    2.0

--- build_files.sh
+++ build_files.sh
@@ -3,9 +3,9 @@
 echo "BUILD START"
 
 # Install Django and project dependencies
-python3.12 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Collect static files
-python3.12 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
