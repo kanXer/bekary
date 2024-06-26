7c387c8bf9ca4e4ae86c76cb1d18e6ab8510e5af
kanxer <user.kanxer@gmail.com>
2024-06-26 08:57:16 +0530

    2.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,6 +129,6 @@ import os
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 # ]
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
